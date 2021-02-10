29382a9a36299154d45cd5346f4f8b18986240f1
Romário Campos <romariocampos.develop@gmail.com>
2021-02-10 19:48:37 -0300

    Introducao a uma API

---
+++ database/seeds/TasksTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TasksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Task::class)->create([
+            'name' => 'Estudar PHP',
+            'complete' => false
+        ]);
+
+        factory(App\Task::class)->create([
+            'name' => 'Estudar JavaScript',
+            'complete' => false
+        ]);
+
+        factory(App\Task::class)->create([
+            'name' => 'Estudar Laravel',
+            'complete' => false
+        ]);
+
+        factory(App\Task::class)->create([
+            'name' => 'Estudar MySQL',
+            'complete' => false
+        ]);
+    }
+}
